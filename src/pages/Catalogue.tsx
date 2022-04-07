699742cd1f4744f84e326169dfc635425588c58b
Fabrice Folituu <fabrice.folituu@lwane.com>
2022-04-07 11:38:08 -1000

    Create Render conditional

--- src/pages/Catalogue.tsx
+++ src/pages/Catalogue.tsx
@@ -1,44 +1,93 @@
 import React from "react";
-import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import {
+  Button,
+  Card,
+  Col,
+  Container,
+  Row,
+  Alert,
+  Spinner,
+} from "react-bootstrap";
 
 const App: React.FC<any> = () => {
   // State
-  const [productList, setProductList] = React.useState<any[]>([])
-  const [isLoading, setIsLoading] = React.useState<boolean>(false)
-  const [hasError, setHasError] = React.useState<boolean>(false)
+  const [productList, setProductList] = React.useState<any[]>([
+    {
+      id: 1,
+      name: "Montre pour enfant",
+      image: "produit1.jpg",
+      description:
+        "Montre Intelligente pour Enfants 7 Jeux - Musique MP3 Montre Enfants Fille Garçon.",
+      price: 1250,
+      stock: 3,
+    },
+  ]);
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [hasError, setHasError] = React.useState<boolean>(false);
 
   // Render
   return (
-    <Container>
-      <Row>
-        <Col>
-          <h4 className="mb-3">Catalogue en ligne</h4>
-        </Col>
-        <Col className="text-end">
-          <span>Total : 1</span>
-        </Col>
-      </Row>
-      <Row>
-        <Col lg={3}>
-          <Card>
-            <Card.Img variant="top" src="https://picsum.photos/100/60" />
-            <Card.Body>
-              <Card.Title>Produit 1</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Card.Text>
-                <strong>
-                  1250<sup>F</sup>
-                </strong>
-              </Card.Text>
-              <Button variant="primary">Ajouter au panier</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      {hasError ? (
+        <Container>
+          <Alert variant="danger" className="text-center">
+            <Alert.Heading>Erreur du serveur</Alert.Heading>
+            <p>Veuillez réessayer plus tard.</p>
+          </Alert>
+        </Container>
+      ) : (
+        <Container>
+          <Row>
+            <Col>
+              <h4 className="mb-3">Catalogue en ligne</h4>
+            </Col>
+            <Col className="text-end">
+              <span>Total : 1</span>
+            </Col>
+          </Row>
+          <Row>
+            {isLoading ? (
+              <div className="text-center mt-5 border py-3">
+                <Spinner
+                  animation="border"
+                  variant="primary"
+                  role="status"
+                  className="mx-auto"
+                >
+                  <span className="visually-hidden">Loading...</span>
+                </Spinner>
+                <p className="text-primary">Chargement des données</p>
+              </div>
+            ) : (
+              <>
+                {productList.map((product) => (
+                  <Col lg={3}>
+                    <Card>
+                      <Card.Img
+                        variant="top"
+                        src={"../src/assets/img/" + product.image}
+                        className="thumbnail p-3"
+                      />
+                      <Card.Body>
+                        <Card.Title>{product.name}</Card.Title>
+                        <Card.Text>{product.description}</Card.Text>
+                        <Card.Text>
+                          <strong>
+                            {product.price}
+                            <sup>F</sup>
+                          </strong>
+                        </Card.Text>
+                        <Button variant="primary">Ajouter au panier</Button>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                ))}
+              </>
+            )}
+          </Row>
+        </Container>
+      )}
+    </>
   );
 };
 
